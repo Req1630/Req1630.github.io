336d5e4aaa4391c03ceb53752038ba7ce8e2b010
Req1630 <46211472+Req1630@users.noreply.github.com>
2020-11-03 22:05:39 +0900

    Update Ball.js

--- Game/BlockBreaking/Ball.js
+++ Game/BlockBreaking/Ball.js
@@ -40,7 +40,7 @@ export default class CBall
 		if( this.isMove == true ) return;
 		this.isMove = true;
 		this.s_x = Math.cos(this.angel);
-		this.s_y = -Math.sin(this.angel);
+		this.s_y = Math.sin(this.angel);
 	}
 	
 	update( canvas, paddle )
