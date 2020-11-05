07908157ae912e800024a92bf1349fe4ca41a0ff
Req1630 <46211472+Req1630@users.noreply.github.com>
2020-11-05 11:06:06 +0900

    Update Ball.js

--- Game/BlockBreaking/Ball.js
+++ Game/BlockBreaking/Ball.js
@@ -73,18 +73,28 @@ export default class CBall
 			this.y += this.s_y;
 			return true;
 		}
-		var c = (this.x - ( paddle.x + paddle.w/2 ))*this.s_x*this.old_s;
+		
+		var c = (this.x - ( paddle.x + paddle.w/2 ))*0.05;
 		if( paddle.x < this.old_x && paddle.x + paddle.w > this.old_x ){
 			this.s_y = -this.s_y;
-			this.s_x = c;
-			this.old_s = -this.old_s;
+			var o = this.old_x - this.x;
+			if( o < 0.0 ){
+				// 左.
+				if( c < 0.0 ){
+					this.s_x = c;
+				}
+			} else {
+				// 右.
+				if( c > 0.0 ){
+					this.s_x = c;
+				}
+			}
 		}
 		else if( paddle.y < this.old_y && paddle.y + paddle.h > this.old_y ){
 			this.s_x = -this.s_x;
 		}
 		else {
-			this.s_x = c;
-			this.old_s = -this.old_s;
+			this.s_x = this.s_x;
 			this.s_y = -this.s_y;
 		}
 
