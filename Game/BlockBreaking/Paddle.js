3215bcbf2714ee74bd28ddb1690ee29195f96e7c
Req1630 <46211472+Req1630@users.noreply.github.com>
2020-11-05 10:20:27 +0900

    Update Paddle.js

--- Game/BlockBreaking/Paddle.js
+++ Game/BlockBreaking/Paddle.js
@@ -3,29 +3,32 @@ export default class CPaddle
     // コンストラクタ.
 	constructor( x, y, w, h, s )
 	{
-		this.x	= x;    // 座標.
-        this.y	= y;    // 座標.
-		this.w	= w;    // 幅.
-		this.h	= h;    // 高さ.
-		this.s	= s;    // 移動速度.
+		this.x		= x;    // 座標.
+		this.y		= y;    // 座標.
+		this.old_x	= x;	// 前回の座標.
+		this.old_y	= y;	// 前回の座標.
+		this.w		= w;    // 幅.
+		this.h		= h;    // 高さ.
+		this.s		= s;    // 移動速度.
     }
     update(canvas, {rightPressed,leftPressed}) 
     {
-        if (rightPressed && this.x < canvas.width - this.w) {
-            this.x += this.s;
-        }
-        if (leftPressed && this.x > 0) {
-            this.x -= this.s;
-        }
+	this.old_x = this.x;
+	if (rightPressed && this.x < canvas.width - this.w) {
+		this.x += this.s;
+	}
+	if (leftPressed && this.x > 0) {
+		this.x -= this.s;
+	}
     }
     draw( ctx )
     {
-        // 矩形の輪郭線の描画開始.
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.w, this.h);
-        ctx.fillStyle = "rgba(50, 100, 255, 0.5)";
-        ctx.fill();	// 輪郭線として指定.
-        // 矩形の輪郭線の描画終了.
-        ctx.closePath();
+	// 矩形の輪郭線の描画開始.
+	ctx.beginPath();
+	ctx.rect(this.x, this.y, this.w, this.h);
+	ctx.fillStyle = "rgba(50, 100, 255, 0.5)";
+	ctx.fill();	// 輪郭線として指定.
+	// 矩形の輪郭線の描画終了.
+	ctx.closePath();
     };
 }
