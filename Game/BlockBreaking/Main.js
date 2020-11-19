3f8da76ec9dbde1cf2b4f119ee41ff0271e994fd
Req1630 <46211472+Req1630@users.noreply.github.com>
2020-11-19 10:34:54 +0900

    Update Main.js

--- Game/BlockBreaking/Main.js
+++ Game/BlockBreaking/Main.js
@@ -40,11 +40,11 @@ function keyUpHandler(e) {
     if( e.keyCode == 32 ){
         ball.shot();
         isStart = true;
-	return false
 	if (e.preventDefault) {
 		e.preventDefault();
 	}
 	e.returnValue = false;
+	return false
     }
 }
 
